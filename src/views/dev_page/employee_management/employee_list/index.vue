221493c8c9483f51ed89b5861948dd632e994e2c
shutao <956275061@qq.com>
2022-11-27 16:13:36 +0800

    IP白名单

--- src/views/dev_page/employee_management/employee_list/index.vue
+++ src/views/dev_page/employee_management/employee_list/index.vue
@@ -1,68 +1,60 @@
 <template>
-    <PageWrapper
-      title="员工列表"
-      contentBackground
-      contentClass="p-2"
-    >
-      <BasicTable @register="registerTable" @edit-change="onEditChange">
-        <template #bodyCell="{ column, record}">
-          <template v-if="column.key === 'action'">
-            <a-button class="mr-1" type="warning" @click="send(record)">编辑</a-button>
-            <PopConfirmButton 
-              class="mr-1" 
-              type="danger" 
-              @confirm="deleteById(record.id)"
-              title="确认删除？">删除</PopConfirmButton>
-            <a-button >权限</a-button>
-          </template>
+  <PageWrapper title="员工列表" contentBackground contentClass="p-2">
+    <BasicTable @register="registerTable" @edit-change="onEditChange">
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'action'">
+          <a-button class="mr-1" type="warning" @click="send(record)">编辑</a-button>
+          <PopConfirmButton type="danger" @confirm="deleteById(record.id)" title="确认删除？">删除</PopConfirmButton>
+          <a-button class="ml-1">权限</a-button>
         </template>
-        <template #toolbar>
-          <a-button type="success" @click="send({})">新增</a-button>
-        </template>
-      </BasicTable>
-      <Modal4 @register = "register4" />
+      </template>
+      <template #toolbar>
+        <a-button type="success" @click="send({})">新增</a-button>
+      </template>
+    </BasicTable>
+    <Modal4 @register="register4" />
   </PageWrapper>
 </template>
 <script lang="ts" setup>
-  import { BasicTable, useTable } from '/@/components/Table';
-  import { demoListApi } from '/@/api/demo/table';
-  import { columns } from './data';
-  import { PopConfirmButton } from '/@/components/Button';
-  import { PageWrapper } from '/@/components/Page';
-  import { useModal } from '/@/components/Modal'
-  import Modal4 from './comp/Modal4.vue';
-  import { useMessage } from '/@/hooks/web/useMessage';
-  let { createMessage } = useMessage()
-  const [registerTable] = useTable({
-    api: demoListApi,
-    columns: columns,
-    bordered: true,
-    showTableSetting: true,
-    // showIndexColumn: false,
-    actionColumn: {
-      width: 220,
-      title: '操作',
-      dataIndex: 'action',
-      // slots: { customRender: 'action' },
-    },
-  });
-  const [register4, { openModal: openModal4}] = useModal();
+import { BasicTable, useTable } from '/@/components/Table';
+import { demoListApi } from '/@/api/demo/table';
+import { columns } from './data';
+import { PopConfirmButton } from '/@/components/Button';
+import { PageWrapper } from '/@/components/Page';
+import { useModal } from '/@/components/Modal'
+import Modal4 from './comp/Modal4.vue';
+import { useMessage } from '/@/hooks/web/useMessage';
+let { createMessage } = useMessage()
+const [registerTable] = useTable({
+  api: demoListApi,
+  columns: columns,
+  bordered: true,
+  showTableSetting: true,
+  // showIndexColumn: false,
+  actionColumn: {
+    width: 220,
+    title: '操作',
+    dataIndex: 'action',
+    // slots: { customRender: 'action' },
+  },
+});
+const [register4, { openModal: openModal4 }] = useModal();
 
-  
-  function send(record:any) {
-      openModal4(true, record);
-    }
 
-  function deleteById(id){
-    createMessage.success('删除成功'+id)
+function send(record: any) {
+  openModal4(true, record);
+}
+
+function deleteById(id) {
+  createMessage.success('删除成功' + id)
+}
+function onEditChange({ column, value, record }) {
+  // 本例
+  if (column.dataIndex === 'id') {
+    record.editValueRefs.name4.value = `${value}`;
   }
-  function onEditChange({ column, value, record }) {
-    // 本例
-    if (column.dataIndex === 'id') {
-      record.editValueRefs.name4.value = `${value}`;
-    }
-    console.log(column, value, record);
-  }
+  console.log(column, value, record);
+}
 
-  
+
 </script>
--- src/views/dev_page/employee_management/ip_whitelist/data.ts
+++ src/views/dev_page/employee_management/ip_whitelist/data.ts
@@ -7,18 +7,8 @@ import {
  
  export const columns: BasicColumn[] = [
   {
-    title: '组别名称',
+    title: 'IP',
     dataIndex: 'name',
-    width: 150,
-  },
-  {
-    title: '状态',
-    dataIndex: 'time',
-    width: 100,
-  },
-  {
-    title: '成员数',
-    dataIndex: 'name5',
     width: 150,
   },
   {
@@ -30,37 +20,11 @@ import {
 
 export const schemas: FormSchema[] = [
   {
-    field: 'name1',
+    field: 'IP',
     component: 'Input',
-    componentProps:{
-      type:'number'
-    },
     defaultValue:'',
     rules: [{ required: true }],
-    label: '组别名称',
-    colProps: {
-      span: 24,
-    },
-  },
-  {
-    field: 'name2',
-    component: 'RadioGroup',
-    rules: [{ required: true }],
-    label: '状态',
-    componentProps: {
-      options: [
-        {
-          label: '启用',
-          value: "0",
-          key: 'VIP0',
-        },
-        {
-          label: '禁用',
-          value: "1",
-          key: 'VIP1',
-        },
-      ],
-    },
+    label: 'IP',
     colProps: {
       span: 24,
     },
@@ -69,7 +33,7 @@ export const schemas: FormSchema[] = [
     field: 'name3',
     component: 'InputTextArea',
     defaultValue:'',
-    rules: [{ required: true }],
+    // rules: [{ required: true }],
     label: '备注',
     colProps: {
       span: 24,
