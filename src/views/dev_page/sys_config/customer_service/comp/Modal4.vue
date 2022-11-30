6f058c07679d34cea5fa7071d0c207e82fea761b
shutao <956275061@qq.com>
2022-11-30 23:03:23 +0800

    1

--- src/views/dev_page/sys_config/customer_service/comp/Modal4.vue
+++ src/views/dev_page/sys_config/customer_service/comp/Modal4.vue
@@ -12,78 +12,78 @@
   </BasicModal>
 </template>
 <script lang="ts">
-  import { defineComponent, ref, nextTick } from 'vue';
-  import { BasicModal, useModalInner } from '/@/components/Modal';
-  import { BasicForm, useForm } from '/@/components/Form/index';
-  import { schemas } from '../data'
+import { defineComponent, ref, nextTick } from 'vue';
+import { BasicModal, useModalInner } from '/@/components/Modal';
+import { BasicForm, useForm } from '/@/components/Form/index';
+import { schemas } from '../data'
 import { useMessage } from '/@/hooks/web/useMessage';
-  export default defineComponent({
-    components: { BasicModal, BasicForm },
-    props: {
-      userData: { type: Object },
-    },
-    setup(props) { // 传过来的值
-      const { createMessage } = useMessage();
-      const modelRef = ref<Recordable>({});
-      const [
-        registerForm,
-        {
-          removeSchemaByField,
-          appendSchemaByField,
-          validate
-        },
-      ] = useForm({
-        labelWidth: 120,
-        schemas:[],
-        showActionButtonGroup: false,
-        actionColOptions: {
-          span: 24,
-        },
-      });
-      const [register,{closeModal}] = useModalInner((data) => {
-        data && onDataReceive(data);
-      });
-      function handleOK(){
-        
-        validate().then(res => {
-          console.log(res);
-          if(res){
-            console.log(modelRef.value);
-            createMessage.success('保存成功'); 
-            closeModal()
-          }
-        }).catch(e => {
-          console.error(e,'dsa');
-          
-        })
+export default defineComponent({
+  components: { BasicModal, BasicForm },
+  props: {
+    userData: { type: Object },
+  },
+  setup(props) { // 传过来的值
+    const { createMessage } = useMessage();
+    const modelRef = ref<Recordable>({});
+    const [
+      registerForm,
+      {
+        removeSchemaByField,
+        appendSchemaByField,
+        validate
+      },
+    ] = useForm({
+      labelWidth: 120,
+      schemas: [],
+      showActionButtonGroup: false,
+      actionColOptions: {
+        span: 24,
+      },
+    });
+    const [register, { closeModal }] = useModalInner((data) => {
+      data && onDataReceive(data);
+    });
+    function handleOK() {
+
+      validate().then(res => {
+        console.log(res);
+        if (res) {
+          console.log(modelRef.value);
+          createMessage.success('保存成功');
+          closeModal()
+        }
+      }).catch(e => {
+        console.error(e, 'dsa');
+
+      })
+    }
+    function onDataReceive(data) { //初始和表单
+      // // 方式2
+      if (data.id == "0") {
+        appendSchemaByField(schemas[0], '')
+        modelRef.value = { field1: data.no, id: data.id };
+      } else {
+        appendSchemaByField(schemas[1], '')
+        modelRef.value = { field2: data.radio1, id: data.id };
       }
-      function onDataReceive(data) { //初始和表单
-         // // 方式2
-         if(data.id == "0"){
-            appendSchemaByField(schemas[0],'')
-            modelRef.value = { field1: data.no,id:data.id};
-          }else{
-            appendSchemaByField(schemas[1],'')
-            modelRef.value = { field2 : data.radio1,id:data.id};
-          }
-          // setProps({
-          //   model:{ field2: data.data, field1: data.info }
-          // })
-      }
-      //监听关闭打开
-      function handleVisibleChange(v) {
-        removeSchemaByField('field1')
-        removeSchemaByField('field2')
-        v && props.userData && nextTick(() => onDataReceive(props.userData));
-      }
-      return { 
-        register, 
-        schemas,
-        registerForm, 
-        modelRef, 
-        handleVisibleChange,
-        handleOK
-      };
-    },
-  });
+      // setProps({
+      //   model:{ field2: data.data, field1: data.info }
+      // })
+    }
+    //监听关闭打开
+    function handleVisibleChange(v) {
+      removeSchemaByField('field1')
+      removeSchemaByField('field2')
+      v && props.userData && nextTick(() => onDataReceive(props.userData));
+    }
+    return {
+      register,
+      schemas,
+      registerForm,
+      modelRef,
+      handleVisibleChange,
+      handleOK
+    };
+  },
+});
 </script>
--- src/views/dev_page/sys_config/customer_service/index.vue
+++ src/views/dev_page/sys_config/customer_service/index.vue
@@ -15,41 +15,41 @@
   </PageWrapper>
 </template>
 <script lang="ts" setup>
-  import { BasicTable, useTable } from '/@/components/Table';
-  import { demoListApi } from '/@/api/demo/table';
-  import { columns } from './data';
-  import { PageWrapper } from '/@/components/Page';
-  import { useModal } from '/@/components/Modal'
-  import Modal4 from './comp/Modal4.vue';
-  
-  const [registerTable] = useTable({
-    api: demoListApi,
-    columns: columns,
-    bordered: true,
-    showTableSetting: true,
-    // showIndexColumn: false,
-    actionColumn: {
-      width: 160,
-      title: 'Action',
-      dataIndex: 'action',
-      // slots: { customRender: 'action' },
-    },
-  });
-  const [register4, { openModal: openModal4}] = useModal();
+import { BasicTable, useTable } from '/@/components/Table';
+import { demoListApi } from '/@/api/demo/table';
+import { columns } from './data';
+import { PageWrapper } from '/@/components/Page';
+import { useModal } from '/@/components/Modal'
+import Modal4 from './comp/Modal4.vue';
 
-  
-  function send(record:any) {
-      openModal4(true, record);
-    }
+const [registerTable] = useTable({
+  api: demoListApi,
+  columns: columns,
+  bordered: true,
+  showTableSetting: true,
+  // showIndexColumn: false,
+  actionColumn: {
+    width: 160,
+    title: 'Action',
+    dataIndex: 'action',
+    // slots: { customRender: 'action' },
+  },
+});
+const [register4, { openModal: openModal4 }] = useModal();
 
 
-  function onEditChange({ column, value, record }) {
-    // 本例
-    if (column.dataIndex === 'id') {
-      record.editValueRefs.name4.value = `${value}`;
-    }
-    console.log(column, value, record);
+function send(record: any) {
+  openModal4(true, record);
+}
+
+
+function onEditChange({ column, value, record }) {
+  // 本例
+  if (column.dataIndex === 'id') {
+    record.editValueRefs.name4.value = `${value}`;
   }
+  console.log(column, value, record);
+}
 
-  
+
 </script>
