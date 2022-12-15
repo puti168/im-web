e62caa4bda162c968749e0bdcc9ba06ddc84f7a6
tangjulei <389702957@qq.com>
2022-12-15 09:17:33 +0800

    修改客服页面

--- src/views/dev_page/online_service/data-ready.js
+++ src/views/dev_page/online_service/data-ready.js
@@ -1,4 +1,13 @@
 export default [
   { name: 'tom', lang: 'en', time: '2011-1-1', id: 1 },
   { name: 'tom', lang: 'en', time: '2011-1-1', id: 2 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 3 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 4 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 5 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 6 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 7 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 8 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 9 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 10 },
+  { name: 'tom', lang: 'en', time: '2011-1-1', id: 11 },
 ];
