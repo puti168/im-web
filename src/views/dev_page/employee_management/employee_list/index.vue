ea82d0b8aedbff28a6104507a16ff6ff7498ef4a
zycxiaoqiao <xiaoqiao@zhuoyan-scm.com>
2022-12-23 12:28:31 +0800

    refactor: 修改密码调用接口,员工管理删除完成刷新页面

--- src/views/dev_page/employee_management/employee_list/index.vue
+++ src/views/dev_page/employee_management/employee_list/index.vue
@@ -1,9 +1,9 @@
 <template>
   <PageWrapper title="员工列表" contentBackground contentClass="p-2">
     <BasicTable @register="registerTable" @edit-change="onEditChange">
-      <template #bodyCell="{ column, record , text}">
+      <template #bodyCell="{ column, record, text }">
         <template v-if="column.key === 'status'">
-          {{{0:'启用',1:'禁用'}[text]}}
+          {{ { 0: '启用', 1: '禁用' }[text] }}
         </template>
         <template v-if="column.key === 'action'">
           <a-button class="mr-1" type="warning" @click="send(record)">编辑</a-button>
@@ -19,50 +19,50 @@
   </PageWrapper>
 </template>
 <script lang="ts" setup>
-import { BasicTable, useTable } from '/@/components/Table';
-import { getUserPageList,deleteUSER } from '/@/api/dev_page/employee_management';
-import { columns } from './data';
-import { PopConfirmButton } from '/@/components/Button';
-import { PageWrapper } from '/@/components/Page';
-import { useModal } from '/@/components/Modal'
-import Modal4 from './comp/Modal4.vue';
-import { useMessage } from '/@/hooks/web/useMessage';
-let { createMessage } = useMessage()
-const [registerTable , {reload}] = useTable({
-  api: getUserPageList,
-  columns: columns,
-  bordered: true,
-  showTableSetting: true,
-  // showIndexColumn: false,
-  actionColumn: {
-    width: 220,
-    title: '操作',
-    dataIndex: 'action',
-    // slots: { customRender: 'action' },
-  },
-});
-const [register4, { openModal: openModal4 }] = useModal();
+  import { BasicTable, useTable } from '/@/components/Table';
+  import { getUserPageList, deleteUSER } from '/@/api/dev_page/employee_management';
+  import { columns } from './data';
+  import { PopConfirmButton } from '/@/components/Button';
+  import { PageWrapper } from '/@/components/Page';
+  import { useModal } from '/@/components/Modal';
+  import Modal4 from './comp/Modal4.vue';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  let { createMessage } = useMessage();
+  const [registerTable, { reload }] = useTable({
+    api: getUserPageList,
+    columns: columns,
+    bordered: true,
+    showTableSetting: true,
+    // showIndexColumn: false,
+    actionColumn: {
+      width: 220,
+      title: '操作',
+      dataIndex: 'action',
+      // slots: { customRender: 'action' },
+    },
+  });
+  const [register4, { openModal: openModal4 }] = useModal();
 
-
-function send(record: any) {
-  openModal4(true, record);
-}
-
-function deleteById(id) {
-  deleteUSER([id]).then(()=>{
-    createMessage.success('删除成功' + id)
-  }).catch(e => {
-    console.error(e);
-    createMessage.success('删除失败' + id)
-  })
-}
-function onEditChange({ column, value, record }) {
-  // 本例
-  if (column.dataIndex === 'id') {
-    record.editValueRefs.name4.value = `${value}`;
+  function send(record: any) {
+    openModal4(true, record);
   }
-  console.log(column, value, record);
-}
 
-
+  function deleteById(id) {
+    deleteUSER([id])
+      .then(() => {
+        reload();
+        createMessage.success('删除成功' + id);
+      })
+      .catch((e) => {
+        console.error(e);
+        createMessage.success('删除失败' + id);
+      });
+  }
+  function onEditChange({ column, value, record }) {
+    // 本例
+    if (column.dataIndex === 'id') {
+      record.editValueRefs.name4.value = `${value}`;
+    }
+    console.log(column, value, record);
+  }
 </script>
