337d3b856da8282429a2245deff29edb46121b72
zycxiaoqiao <xiaoqiao@zhuoyan-scm.com>
2022-12-29 17:32:11 +0800

    refactor: 对话详情的状态调整

--- src/views/dev_page/history_record/dialogue_details.vue
+++ src/views/dev_page/history_record/dialogue_details.vue
@@ -4,7 +4,7 @@
       <template #bodyCell="{ column, record, text }">
         <template v-if="column.key === 'id'"> ID: {{ record.id }} </template>
         <template v-if="column.key === 'status'">
-          {{ {0:'等待中',1:'服务中',2:'超时结束',3:'正常结束'}[Number(text)] || text }}
+          {{ statusName[Number(text)] || text }}
         </template>
         <template v-if="column.key === 'msgNum'">
           <a @click="clickNumber(record)">{{ text }}</a>
@@ -20,16 +20,16 @@
       </template>
       <template #bodyCell="{ column, record, text }">
         <template v-if="column.key === 'msgType'">
-          {{ 
-              {
-                0:'心跳消息',
-                1:'文字消息',
-                2:'图片',
-                3:'视频',
-                4:'语音',
-                5:'心跳消息',
-                6:'通知用户匹配成功',
-              }[Number(text)] 
+          {{
+            {
+              0: '心跳消息',
+              1: '文字消息',
+              2: '图片',
+              3: '视频',
+              4: '语音',
+              5: '心跳消息',
+              6: '通知用户匹配成功',
+            }[Number(text)]
           }}
         </template>
         <template v-if="column.key === 'createTime'">
@@ -45,30 +45,39 @@
 </template>
 <script lang="ts" setup>
   import { BasicTable, useTable, TableImg } from '/@/components/Table';
-  import { getSessionorderList,getSessionorderListMsg } from '/@/api/dev_page/history_record';
-  import { columns,columnsAll } from './dialogue_details_table';
+  import { getSessionorderList, getSessionorderListMsg } from '/@/api/dev_page/history_record';
+  import { columns, columnsAll } from './dialogue_details_table';
   import { ref } from 'vue';
+  const statusName = {
+    1: '等待中',
+    2: '服务中',
+    3: '会话结束，客服关闭',
+    4: '客户未被匹配上客服，客户关闭',
+    5: '会话结束，客户关闭',
+    6: '用户超过一个小时未回复关闭订单',
+    7: '多次创建会话，历史会话关闭',
+  };
 
-  const [registerTable ] = useTable({
+  const [registerTable] = useTable({
     api: getSessionorderList,
     columns: columnsAll,
 
     bordered: true,
     showTableSetting: true,
   });
-  let sessionOrderId = ref<string>('112333')
+  let sessionOrderId = ref<string>('112333');
   const [registerTableMsg] = useTable({
     api: getSessionorderListMsg,
     columns: columns,
-    searchInfo:{
-      sessionOrderId:sessionOrderId,
-      sortByTime:1,
+    searchInfo: {
+      sessionOrderId: sessionOrderId,
+      sortByTime: 1,
     },
     bordered: true,
     showTableSetting: true,
   });
-  let isMessage = ref<boolean>(false)
-  function clickNumber(item:any){
+  let isMessage = ref<boolean>(false);
+  function clickNumber(item: any) {
     isMessage.value = true;
     sessionOrderId.value = item.id;
   }
