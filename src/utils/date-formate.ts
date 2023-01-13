8217bee28fe55a84b9915d1b2c06a8a1953d4ff8
carl <carl@gmail.com>
2023-01-13 15:24:34 +0800

    feat: 时间-1兼容

--- src/utils/date-formate.ts
+++ src/utils/date-formate.ts
@@ -3,11 +3,13 @@ import moment from 'moment';
 const initVal = init();
 
 export function formateTime(time: number | string): string {
+  if (time === -1) return '-';
   time = typeof time === 'number' ? time : new Date(time).getTime();
   return moment(time).utcOffset(initVal).format('YYYY-MM-DD HH:mm:ss');
 }
 
 export function formateTimeDate(time: number): string {
+  if (time === -1) return '-';
   time = typeof time === 'number' ? time : new Date(time).getTime();
   return moment(time).utcOffset(initVal).format('YYYY-MM-DD');
 }
